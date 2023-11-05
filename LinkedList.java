e397a29535d6da9fc5eca9770dba9ee76efff121
JayeshGajarkar <jayeshgajarkar@gmail.com>
2023-11-05 22:27:12 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,5 @@
 public class LinkedList{
-    public static void main(String[] arge){
-        System.out.print("Hello");
+    public static void main(String[] args){
+        System.out.print("Hello world");
     }
 }
